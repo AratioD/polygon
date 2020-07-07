2918537caf726d49962d22a263e9430e5ca9af4e
AratioD <fffb@ddd.com>
2020-07-07 22:34:40 +0300

    basic settings are setted

---
+++ polygon.py
@@ -0,0 +1,12 @@
+
+class Polygon:
+    
+    def __init__(self, edges, circumradius):
+        self._edges = edges
+        self._circumradius = circumradius
+        
+def main():
+    print("hellooo")
+
+if __name__ == "__main__":
+    main()
