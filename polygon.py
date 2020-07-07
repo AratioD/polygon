6bb9507f4a6e6f8b5d4e6097c1e75109d34a56b7
AratioD <fffb@ddd.com>
2020-07-07 22:46:06 +0300

    doctstring implementation

--- polygon.py
+++ polygon.py
@@ -1,12 +1,21 @@
 
 class Polygon:
-    
+    """Class """
+
     def __init__(self, edges, circumradius):
         self._edges = edges
         self._circumradius = circumradius
-        
+
+
 def main():
-    print("hellooo")
+
+    # n edges
+    n = 4
+    # R circumradius
+    R = 40
+
+    poly = Polygon(n, R)
+
 
 if __name__ == "__main__":
     main()
