76788abc87fab442ab58a765227791f77c67e0de
AratioD <fffb@ddd.com>
2020-07-08 10:16:04 +0300

    setters and getters added to edges

--- polygon.py
+++ polygon.py
@@ -7,9 +7,19 @@ class Polygon:
     def __init__(self, edges: int, circumradius: int) -> int:
         self._edges = edges
         self._circumradius = circumradius
+    
+    @property
+    def edges(self):
+        return self._edges
+    
+    @edges.setter
+    def edges(self, a):
+        if a <= 2:
+            raise ValueError(f"Edges amount needs to be higher than {a}")
 
 
 def main():
+    """The main class"""
 
     # n edges
     n = 4
